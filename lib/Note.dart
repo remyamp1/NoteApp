f8d57229b99b9e663e2b173cded8d82c1bb9ed39
remyamp <remyamp774@gmail.com>
2024-10-17 12:39:43 +0530

    "17-10-24"

--- lib/Note.dart
+++ lib/Note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 class NoteApp extends StatefulWidget {
   
 
@@ -7,6 +8,26 @@ class NoteApp extends StatefulWidget {
 }
 
 class _NoteAppState extends State<NoteApp> {
+  late Box box;
+   List<Map<String,String>> itemList=[];
+
+  TextEditingController titleController=TextEditingController();
+  TextEditingController categortController=TextEditingController();
+  TextEditingController descriptionController=TextEditingController();
+  TextEditingController dateController=TextEditingController();
+ 
+  @override
+  void initState(){
+    super.initState();
+     box=Hive.box("MyBox");
+     final storedItems=box.get('itemsList');
+     if(storedItems is List){
+      itemList=List<Map<String,String>>.from(
+        storedItems.map((e)=>Map<String,String>.from(e))
+      );
+     }
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,17 +35,22 @@ class _NoteAppState extends State<NoteApp> {
         title: Text('NOTEAPP'),backgroundColor: Colors.blue,
       ),
 body: 
-GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
- itemBuilder: (context,index){
+GridView.builder(
+  itemCount: itemList.length,
+  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+ itemBuilder: (
+  
+  context,index){
+  final item=itemList[index];
   return Container(
     height: 300,
     width: double.infinity,
     child: Column(
       children: [
-        Text("fghgh"),
-        Text('ghbv'),
-        Text('gfgsg'),
-        Text('fgfhgh')
+        Text(item['title']?? 'No title'),
+        Text(item['category']?? 'No catrgory'),
+        Text(item['description']?? 'No description'),
+        Text(item['date']?? 'No date')
       ],
     ),
   );
@@ -32,6 +58,7 @@ GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAx
 
 floatingActionButton: FloatingActionButton(onPressed: (){
       showModalBottomSheet(context: context, builder: (BuildContext context){
+        
         return Container(
 height: 400,
 width: double.infinity,
@@ -42,32 +69,61 @@ child: Padding(
       SizedBox(
         height: 50,
         width: 300,
-        child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Title')),)),
+        child: TextField(
+          controller: titleController,
+          decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Title')),)),
       SizedBox(height: 10),
        SizedBox(
         height:50,
         width: 300,
-        child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Categort')),)),
+        child: TextField(
+          controller: categortController,
+          decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Categort')),)),
       SizedBox(height: 10),
        SizedBox(
         height: 50,
         width: 300,
-        child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Description')),)),
+        child: TextField(
+          controller: descriptionController,
+          decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Description')),)),
       SizedBox(height: 10),
        SizedBox(
         height: 50,
         width: 300,
-        child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Date')),)),
+        child: TextField(
+          controller: dateController,
+          decoration: InputDecoration(border: OutlineInputBorder(),label:Text('Date')),)),
         SizedBox(height: 10),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             TextButton(onPressed: (){
+              setState(() {
+                itemList.add({'title':titleController.text,
+                'category':categortController.text,
+                'description':descriptionController.text,
+                'date':dateController.text,
 
-            }, child: Text('Add')),
+              });
+              box.put(
+                'itemsList',
+                 itemList.map((e)=>Map<String,dynamic>.from(e)
+                ).toList());
+            });
+              titleController.clear();
+              categortController.clear();
+              descriptionController.clear();
+              dateController.clear();
+            Navigator.pop(context);
+            },
+
+             child: Text('Add')
+             ),
             SizedBox(width: 5),
-              TextButton(onPressed: (){}, child: Text('Cancle')),
+              TextButton(onPressed: (){
+                Navigator.of(context);
+              }, child: Text('Cancle')),
           ],
         )
       
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:noteapp/Note.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  var box=await Hive.openBox('mybox');
   runApp(const MyApp());
 }
 
